3fa43c03fd062154cd3f9fc53c2c19addd3bd097
Alejosegarra <63474034+Alejosegarra@users.noreply.github.com>
2025-08-21 17:08:29 -0300

    Delete optica-schellhas---seguimiento-de-trabajos (3) directory

--- optica-schellhas---seguimiento-de-trabajos (3)/types.ts
+++
@@ -1,9 +0,0 @@
-import type { User } from './services/database.types.ts';
-
-export * from './services/database.types.ts';
-
-export interface AuthContextType {
-  currentUser: User | null;
-  login: (username: string, password: string) => Promise<User | null>;
-  logout: () => void;
-}
