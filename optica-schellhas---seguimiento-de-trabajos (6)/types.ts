6df99258fd981c921dd1872bea739e98f42327ec
Alejosegarra <63474034+Alejosegarra@users.noreply.github.com>
2025-08-21 17:21:58 -0300

    Add files via upload

---
+++ optica-schellhas---seguimiento-de-trabajos (6)/types.ts
@@ -0,0 +1,5 @@
+import type { User } from './services/database.types.ts';
+
+export * from './services/database.types.ts';
+
+// La interfaz AuthContextType ha sido movida a contexts/AuthContext.tsx para estar junto a su implementación.
