21671ff08df4d2dcb9740c503fc4b1c33f4c5774
Alejosegarra <63474034+Alejosegarra@users.noreply.github.com>
2025-08-22 18:56:55 -0300

    Delete optica-schellhas---seguimiento-de-trabajos (17) directory

--- optica-schellhas---seguimiento-de-trabajos (17)/types.ts
+++
@@ -1,5 +0,0 @@
-import type { User } from './services/database.types.ts';
-
-export * from './services/database.types.ts';
-
-// La interfaz AuthContextType ha sido movida a contexts/AuthContext.tsx para estar junto a su implementación.
